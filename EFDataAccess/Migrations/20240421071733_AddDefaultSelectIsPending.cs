5ea7b37f1b91ea45a9344178711999168a10ae92
DavidGPC <dcontre10@gmail.com>
2024-04-21 02:21:27 -0600

    add db field ford default user options

---
+++ EFDataAccess/Migrations/20240421071733_AddDefaultSelectIsPending.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultSelectIsPending : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DefaultSelectIsPending",
+                table: "Account",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultSelectIsPending",
+                table: "Account");
+        }
+    }
+}
